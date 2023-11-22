273385265497d86156b9eff9f1eb0a5cb7b18144
Marcio <marciocad@gmail.com>
2023-11-22 09:01:03 -0300

    Incorporada a classe Sanitizacao no mesmo arquivo para evitar validacao de assinatura de arquivo externo do powershell

--- GetTranscripYt.ps1
+++ GetTranscripYt.ps1
@@ -1,25 +1,118 @@
+<#
+******* Class TranscriptionSanitization *******
+#>
 
+Class TranscriptionSanitization{
+    [string[]] $outputTranscription
+    [string] $fileName
+    [bool] $isNextLineRepeat = $false
+    [string] $lastLineValue = ""
+    [ref] $refOutputTranscription #[PSreference]
+
+    TranscriptionSanitization($pathFileName){
+        if($pathFileName -eq ""){
+            Write-Warning "Empty file."
+        }
+        if(Test-Path $pathFileName){
+            $this.fileName = $pathFileName
+            $this._setOutputTranscription()
+        } else {
+            Write-Warning "The $pathFileName is invalid."
+        }
+    }
+
+    #return a reference to array of strings
+    [ref] getOutput(){
+        return $this.refOutputTranscription
+    }
+
+    [void] _setOutputTranscription(){
+        [string[]] $content = @()
+        [bool] $isInValidLine = $false
+
+        $content = Get-Content -Path  $this.fileName
+        $this.outputTranscription = foreach($line in $content){
+
+            $isInValidLine = $this._invalidPattern($line)
+            if($isInValidLine){continue}
+
+            $this._checkRepeateLine($line)
+            if(!$this.isNextLineRepeat){
+                Write-Output $line
+            }
+        }
+        $this.refOutputTranscription = $this.outputTranscription
+    }
+
+    [void] _checkBugTimeWrite($line){
+        #pattern sample 00:00:02,000 --> 00:00:04,550
+        [string] $timePattern = "[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"
+        
+        if($line -match $timePattern){
+
+            $m = Select-String "[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}" -AllMatches -InputObject $line
+            [TimeSpan]$t1 = [TimeSpan]::Parse($m.Matches[0].Value)
+            [TimeSpan]$t2 = [TimeSpan]::Parse($m.Matches[1].Value)
+            [TimeSpan]$diffTime = $t2 - $t1
+
+            #if the time was write between 20ms or less
+            if($diffTime.TotalMilliseconds -le 20){
+                Write-Host "The next line valid value will be the same"
+                
+            } else {
+                
+            }
+        }
+    }
+
+    [void] _checkRepeateLine($line){
+        
+        if($this.lastLineValue -ne $line){
+            $this.isNextLineRepeat = $false
+            $this.lastLineValue = $line
+        } else {
+            $this.isNextLineRepeat = $true
+        }
+
+    }
+
+    [bool] _invalidPattern($line){
+        [string] $timePattern = "[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"
+        [string] $numberPattern = "[0-9]{1,6}" #line numbers
+        [string] $emptyPattern = "^$"
+        [string] $oneSpacePattern = "^ $"
+        [string] $noiseDescriptionPattern = "^\(.*\)$" #Any char, since start "(" and end ")"
+        [string] $fullPattern = "($timePattern)|($numberPattern)|($emptyPattern)|($oneSpacePattern)|($noiseDescriptionPattern)"
+
+        return $line -match $fullPattern
+    }
+}
+
+<#
+******* Class YtTranscription *******
+#>
 Class YtTranscription{
     [string] $ytUrl
     [object] $out
     [string] $fileName
     [string] $fileNameExtension
-    [string[]] $outputTranscription
+    [ref] $outputTranscription
     
     YtTranscription($ytUrl){
         $this.ytUrl = $ytUrl
         $this.fileNameExtension = "srt"
+        [ref] $this.outputTranscription = @()
         $this._generateRawTranscription()
         $this._sanitizeTranscription()
     }
 
     [void] saveTranscriptionInTxtFile(){
-        $this.outputTranscription | Out-File "result.txt"
+        $this.outputTranscription.Value | Out-File "result.txt"
         Write-Host "Saved in result.txt"
     }
 
     [void] sendTranscriptionToClipboard(){
-        $this.outputTranscription | Set-Clipboard
+        $this.outputTranscription.Value | Set-Clipboard
         Write-Host "Copied to Clippboard"
     }
 
@@ -27,27 +120,14 @@ Class YtTranscription{
         [string[]] $prompt = @()
         $prompt = Get-Content -Path ".\prompt-ChatGPT.txt"
         #join arrays and send to clipboard
-        $prompt + $this.outputTranscription | Set-Clipboard 
+        $prompt + $this.outputTranscription.Value | Set-Clipboard 
         Write-Host "ChatGPT prepared"
     }
 
     [void] _sanitizeTranscription(){
         [string] $filePath = -join(".\", $this.fileName)
-        [string[]] $content = @()
-        $content = Get-Content -Path  $filePath
-
-        [string] $timePattern = "[0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3} --> [0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}"
-        [string] $numberPattern = "[0-9]{1,6}" #line numbers
-        [string] $emptyPattern = "^$"
-        [string] $noiseDescriptionPattern = "^\(.*\)$" #Any char, since start "(" and end ")"
-        [string] $fullPattern = "($timePattern)|($numberPattern)|($emptyPattern)|($noiseDescriptionPattern)"
-        
-        $this.outputTranscription = foreach($line in $content){
-            $isMatch = $line -match $fullPattern
-            if($isMatch) {continue}
-            Write-Output $line
-        }
-
+        $sanitizer = [TranscriptionSanitization]::new($filePath)
+        $this.outputTranscription = $sanitizer.getOutput()
     }
 
     [void] _generateRawTranscription(){
